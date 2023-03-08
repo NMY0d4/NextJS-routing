f9f4cb9b926ffbe2c22f2e76e0cfb94c8083b995
Grégory Marini <greg.marini@hotmail.fr>
2023-03-08 18:50:25 +0100

    review basic routing

---
+++ pages/portfolio/[projectid].js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+export default function PortfolioProjectPage() {
+  const router = useRouter();
+
+  console.log(router.pathname, router.query);
+
+  // send a request to some backend server
+  // to fetch the piece of data with an id of router.query.projectid
+
+  return (
+    <div>
+      <h1>The Portfolio Project Page</h1>
+    </div>
+  );
+}
