a4a38cbc1150e23b5ca273c7f0c5db8968c8060d
Grégory Marini <greg.marini@hotmail.fr>
2023-03-10 18:43:58 +0100

    add 404 and button router(client file)

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 export default function ClientsProjectsPage() {
+  const router = useRouter();
+
+
+  function loadProjectHandler() {
+    // load data...
+    router.push('/clients/max/projecta');
+  }
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
