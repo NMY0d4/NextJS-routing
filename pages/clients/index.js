403d6112dea272f5de6f110a0a06df6b7b728f90
Grégory Marini <greg.marini@hotmail.fr>
2023-03-10 16:43:00 +0100

    last update

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,9 +1,26 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default function ClientsPage() {
+  const clients = [
+    { id: 'greg', name: 'Grégory' },
+    { id: 'max', name: 'Maximilian' },
+    { id: 'dani', name: 'Daniel' },
+  ];
   return (
     <div>
       <h1>My clients page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link
+              href={{ pathname: '/clients/[id]', query: { id: client.id } }}
+            >
+              {client.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
 
 export default function HomePage() {
-  return <div>
-    <h1>The Home Page</h1>
-  </div>;
+  return (
+    <div>
+      <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href='/portfolio'>Portfolio</Link>
+        </li>
+        <li>
+          <Link href='/clients'>Clients</Link>
+        </li>
+        <li></li>
+      </ul>
+    </div>
+  );
 }
